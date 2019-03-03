6a1162a9c5a62f0280eefaa341c2d8d69e043c29
Ekanath Reddy <kcsuchireddy>
2019-03-03 21:32:32 +0000

    first commit of howework project

---
+++ src/love/programming/MyHomeWorkMain.java
@@ -0,0 +1,13 @@
+package love.programming;
+
+public class MyHomeWorkMain {
+    public static void main(String[] args) {
+MyHomeWorkClass1  MHWC1_obj1 = new MyHomeWorkClass1();
+MyHomeWorkClass1  MHWC1_obj2 = new MyHomeWorkClass1();
+
+MyHomeWorkClass2 MHWC2_obj1 = new MyHomeWorkClass2();
+MyHomeWorkClass2 MHWV2_obj2 = new MyHomeWorkClass2();
+
+
+    }
+}
